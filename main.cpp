17fd73ae9ab55b08c80ac6d816d87071176a82f0
luciana1faur <luciana_faur@yahoo.com>
2020-09-21 07:13:55 +0000

    Structuri de date Produs... fructe[i]

--- main.cpp
+++ main.cpp
@@ -1,5 +1,34 @@
-#include <iostream>
+# include <iostream>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main(){
+  
+  struct produs{
+    int greutate;
+    double pret;
+    char nume[15];
+  };
+
+  produs fructe[10];
+  int nr_produse;
+  int i;
+  i = 0;
+
+  cout << "Cate produse introduceti? ";
+  cin >> nr_produse;
+
+  while(i < nr_produse){
+    cout << "Greutatea produsului: ";
+    cin >> fructe[i].greutate;
+
+    cout << "Pretul produsului: ";
+    cin >> fructe[i].pret;
+
+    cout << "Numele produsului: ";
+    cin.ignore();
+    cin.getline(fructe[i].nume, 14);
+
+    i++;
+  }
+  return 0;
 }
